2074df4aeeb23ccc66e9aa1cf76a67581229546f
hershRVA <herchenrother@gmail.com>
2023-06-20 16:08:22 -0400

    added functions to Java

--- Develop/script.js
+++ Develop/script.js
@@ -2,6 +2,8 @@ var timeDisplayEl = $('#currentDay');
 var timeBlockEl = $('.time-block');
 var saveBtn = $('.saveBtn')
 
+$(document).ready(function () {
+
 function displayTime() {
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
   timeDisplayEl.text(rightNow);
@@ -9,7 +11,6 @@ function displayTime() {
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(document).ready(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -28,7 +29,7 @@ $(document).ready(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+
 
 function checkTime() {
   timeBlockEl.each(function () {
@@ -57,18 +58,21 @@ function checkTime() {
   });
 }
 
+var timeBlock;
+var inputText;
+
 function handleEventSubmit(event) {
   event.preventDefault();
-  console.log("handle ", event.target.parent.getAttribute('id'));
+  console.log("handle ", $(event.target).parent().attr('id'));
 
-  var timeBlock =  $(this).parent().getAttribute('id');
+  var timeBlock = $(event.target).parent().attr('id');
   console.log(timeBlock);
-  var inputText = $(this).siblings(".description").val();
+  var inputText = $(event.target).siblings(".description").val();
   console.log(inputText);
 
   var newEvent = {
-      time: timeBlock,
-      event: inputText,
+    time: timeBlock,
+    event: inputText,
   };
 
   var events = readEventsFromStorage();
@@ -76,13 +80,64 @@ function handleEventSubmit(event) {
   console.log('here is events: ', events);
   saveEventsToStorage(events);
 
-  printEventData();
+  readEventsFromStorage();
 }
 
+function saveEventsToStorage(events) {
+  localStorage.setItem(timeBlock, JSON.stringify(events));
+}
 
+function readEventsFromStorage() {
+  console.log(events);
+
+  var readEvent8 = JSON.parse(localStorage.getItem("hour-8"));
+  $('#hour-8').children[1].val('');
+  $('#hour-8').children[1].val(readEvent8);
+  
+  var readEvent9 = JSON.parse(localStorage.getItem("hour-9"));
+  $('#hour-9').children[1].val('');
+  $('#hour-9').children[1].val(readEvent9);
+  
+  var readEvent10 = JSON.parse(localStorage.getItem("hour-10"));
+  $('#hour-10').children[1].val('');
+  $('#hour-10').children[1].val(readEvent10);
+  
+  var readEvent11 = JSON.parse(localStorage.getItem("hour-11"));
+  $('#hour-11').children[1].val('');
+  $('#hour-11').children[1].val(readEvent11);
+  
+  var readEvent12 = JSON.parse(localStorage.getItem("hour-12"));
+  $('#hour-12').children[1].val('');
+  $('#hour-12').children[1].val(readEvent12);
+  
+  var readEvent13 = JSON.parse(localStorage.getItem("hour-13"));
+  $('#hour-13').children[1].val('');
+  $('#hour-13').children[1].val(readEvent13);
+  
+  var readEvent14 = JSON.parse(localStorage.getItem("hour-14"));
+  $('#hour-14').children[1].val('');
+  $('#hour-14').children[1].val(readEvent14);
+  
+  var readEvent15 = JSON.parse(localStorage.getItem("hour-15"));
+  $('#hour-15').children[1].val('');
+  $('#hour-15').children[1].val(readEvent15);
+  
+  var readEvent16 = JSON.parse(localStorage.getItem("hour-16"));
+  $('#hour-16').children[1].val('');
+  $('#hour-16').children[1].val(readEvent16);
+  
+  var readEvent17 = JSON.parse(localStorage.getItem("hour-17"));
+  $('#hour-17').children[1].val('');
+  $('#hour-17').children[1].val(readEvent17);
+
+}
 
 saveBtn.addEventListener("click", handleEventSubmit);
 
 displayTime();
 checkTime();
 setInterval(displayTime, 1000);
+
+readEventsFromStorage();
+
+});
