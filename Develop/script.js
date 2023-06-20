8069cbcaffe68b089be81128ed9226b28368978e
hershRVA <herchenrother@gmail.com>
2023-06-20 17:25:03 -0400

    changes to java

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,7 @@
 var timeDisplayEl = $('#currentDay');
 var timeBlockEl = $('.time-block');
 var saveBtn = $('.saveBtn')
+var events = [];
 
 $(document).ready(function () {
 
@@ -63,76 +64,31 @@ var inputText;
 
 function handleEventSubmit(event) {
   event.preventDefault();
-  console.log("handle ", $(event.target).parent().attr('id'));
+  console.log("handle ", $(this).parent().attr('id'));
 
-  var timeBlock = $(event.target).parent().attr('id');
+  timeBlock = $(this).parent().attr('id');
   console.log(timeBlock);
-  var inputText = $(event.target).siblings(".description").val();
+  inputText = $(this).siblings(".description").val();
   console.log(inputText);
 
-  var newEvent = {
-    time: timeBlock,
-    event: inputText,
-  };
+  saveEventsToStorage();
+}
 
-  var events = readEventsFromStorage();
-  events.push(newEvent);
-  console.log('here is events: ', events);
-  saveEventsToStorage(events);
-
+function saveEventsToStorage() {
+  localStorage.setItem(timeBlock, JSON.stringify(inputText));
   readEventsFromStorage();
 }
 
-function saveEventsToStorage(events) {
-  localStorage.setItem(timeBlock, JSON.stringify(events));
-}
-
 function readEventsFromStorage() {
-  console.log(events);
-
-  var readEvent8 = JSON.parse(localStorage.getItem("hour-8"));
-  $('#hour-8').children[1].val('');
-  $('#hour-8').children[1].val(readEvent8);
+  var hours = ["hour-8", "hour-9", "hour-10", "hour-11", "hour-12", "hour-13", "hour-14", "hour-15", "hour-16", "hour-17"];
   
-  var readEvent9 = JSON.parse(localStorage.getItem("hour-9"));
-  $('#hour-9').children[1].val('');
-  $('#hour-9').children[1].val(readEvent9);
-  
-  var readEvent10 = JSON.parse(localStorage.getItem("hour-10"));
-  $('#hour-10').children[1].val('');
-  $('#hour-10').children[1].val(readEvent10);
-  
-  var readEvent11 = JSON.parse(localStorage.getItem("hour-11"));
-  $('#hour-11').children[1].val('');
-  $('#hour-11').children[1].val(readEvent11);
-  
-  var readEvent12 = JSON.parse(localStorage.getItem("hour-12"));
-  $('#hour-12').children[1].val('');
-  $('#hour-12').children[1].val(readEvent12);
-  
-  var readEvent13 = JSON.parse(localStorage.getItem("hour-13"));
-  $('#hour-13').children[1].val('');
-  $('#hour-13').children[1].val(readEvent13);
-  
-  var readEvent14 = JSON.parse(localStorage.getItem("hour-14"));
-  $('#hour-14').children[1].val('');
-  $('#hour-14').children[1].val(readEvent14);
-  
-  var readEvent15 = JSON.parse(localStorage.getItem("hour-15"));
-  $('#hour-15').children[1].val('');
-  $('#hour-15').children[1].val(readEvent15);
-  
-  var readEvent16 = JSON.parse(localStorage.getItem("hour-16"));
-  $('#hour-16').children[1].val('');
-  $('#hour-16').children[1].val(readEvent16);
-  
-  var readEvent17 = JSON.parse(localStorage.getItem("hour-17"));
-  $('#hour-17').children[1].val('');
-  $('#hour-17').children[1].val(readEvent17);
-
+  hours.forEach(function(hour) {
+    var readEvent = JSON.parse(localStorage.getItem(hour));
+    $('#' + hour).children('.description').val(readEvent || '');
+  });
 }
 
-saveBtn.addEventListener("click", handleEventSubmit);
+saveBtn.click(handleEventSubmit);
 
 displayTime();
 checkTime();
